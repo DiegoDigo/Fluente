bcfd9e09807acdd19d8709bac8ef4909a4103360
Diego Delmiro <di3g0d0ming05@gmail.com>
2020-10-17 14:33:59 -0300

    fix: ajuste no teste de notificaçaõ

--- src/test/java/notification/NotificationTest.java
+++ src/test/java/notification/NotificationTest.java
@@ -2,7 +2,6 @@ package notification;
 
 
 import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import validation.Contract;
